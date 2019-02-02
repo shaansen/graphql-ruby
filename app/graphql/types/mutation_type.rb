4b54e65467088749dc7465b8b8882a2b43dd11f1
shaansen <shantanu.sengupta2008@gmail.com>
2019-02-02 18:36:59 -0500

    Updated the Mutation Type and added a CreateLink Test

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,43 @@
 module Types
-  class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+  class MutationType < BaseObject
+    field :create_link, mutation: Mutations::CreateLink
+  end
+end```
+
+</Instruction>
+
+### Testing with Playground
+
+To test, just restart the server again and use the new mutation with GraphiQL:
+
+![](http://i.imgur.com/pHNRZlG.png)
+
+### Testing with Unit Test
+
+It's a good practice in Ruby to unit test your resolver objects.
+
+Here is an example of `Resolvers::CreateLink` test:
+
+```ruby(path=".../graphql-ruby/test/graphql/mutations/create_link_test.rb")
+require 'test_helper'
+
+class Mutations::CreateLinkTest < ActiveSupport::TestCase
+  def perform(user: nil, **args)
+    Mutations::CreateLink.new(object: nil, context: {}).resolve(args)
+  end
+
+  test 'create a new link' do
+    user = create :user
+
+    link = perform(
+      url: 'http://example.com',
+      description: 'description',
+      user: user
+    )
+
+    assert link.persisted?
+    assert_equal link.description, 'description'
+    assert_equal link.url, 'http://example.com'
+    assert_equal link.user, user
   end
 end
