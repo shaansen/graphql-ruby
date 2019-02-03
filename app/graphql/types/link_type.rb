c9a3c8bfb42d27ef62489cb59d8c11d91737c1ec
shaansen <shantanu.sengupta2008@gmail.com>
2019-02-03 00:12:52 -0500

    Added code for authentication

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -1,8 +1,10 @@
 module Types
-	class LinkType < BaseObject
-		field :id, ID, null: false
-		field :created_at, DateTimeType, null: false
-		field :url, String, null: false
-		field :description, String, null: false
-	end
+  class LinkType < BaseObject
+    field :id, ID, null: false
+    field :created_at, DateTimeType, null: false
+    field :url, String, null: false
+    field :description, String, null: false
+    # "method" option remaps field to an attribute of Link model
+    field :posted_by, UserType, null: false, method: :user
+  end
 end
