2d49e6c1156cdcd880743f69e02ab633e1cc04ee
shaansen <shantanu.sengupta2008@gmail.com>
2019-02-02 21:01:21 -0500

    created mutation for links
    Resolvers can be either of two things:
    
    object responding to call method accepting 3 arguments - obj, arg, ctx (like lambda or Proc objects)
    
    GraphQL::Function - we are going discuss those in the next chapter
    
    In this commit, we use the resolver Type for createLink to be a GraphQL::Function

--- app/graphql/mutations/create_link.rb
+++ app/graphql/mutations/create_link.rb
@@ -8,11 +8,8 @@ module Mutations
     def resolve(description: nil, url: nil)
       Link.create!(
         description: description,
-        url: url,
-        user: context[:current_user]
+        url: url, 
       )
-    rescue ActiveRecord::RecordInvalid => e
-      GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
     end
   end
 end
