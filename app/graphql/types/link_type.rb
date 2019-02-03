78a0ad4fd78315947e95b776b2fc8d3c277361a9
shaansen <shantanu.sengupta2008@gmail.com>
2019-02-02 20:53:12 -0500

    setup graphql and created link queries
    Added Graph QL Gems
    Setup GraphQL Directories
    Created GraphQL Query Types and Resolvers

---
+++ app/graphql/types/link_type.rb
@@ -0,0 +1,8 @@
+module Types
+	class LinkType < BaseObject
+		field :id, ID, null: false
+		field :created_at, DateTimeType, null: false
+		field :url, String, null: false
+		field :description, String, null: false
+	end
+end
